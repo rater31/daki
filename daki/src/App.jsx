55b3a808c8685676a904e91c6dafcbfc0292cd43
rater31 <fedehidalgo89@gmail.com>
2024-12-17 21:13:21 -0300

    fede

--- daki/src/App.jsx
+++ daki/src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import Carrusel from './components/Carrusel'
 import Nav from './components/Nav'
 import Cards from './components/Cards'
-import productos from './productosDestacados.json'
+import productosDestacados from './productosDestacados.json'
 import Ofertas from './components/Ofertas'
+import Productos from './components/Productos'
+import productos from './productos.json'
 
 
 function App() {
@@ -13,11 +15,18 @@ function App() {
      <Nav></Nav>
      <Carrusel/>
     <div className='flex justify-around'>
-     {productos.map(item => (
+     {productosDestacados.map(item => (
       <Cards titulo={item.name} imagen={item.imagen} precio={item.price}/>
      ))}
     </div>
     <Ofertas/>
+    <div className='grid grid-cols-3 gap-4 mx-5'>
+
+      {productos.map(items =>(
+          
+          <Cards  titulo={items.name} imagen={items.imagen} precio={items.price}/>
+      ))}
+    </div>
     </>
   )
 }
